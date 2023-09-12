781b476e7f15955ec16a4bf255dd2774f16cf923
Hectorvejarc <144828077+Hectorvejarc@users.noreply.github.com>
2023-09-12 11:29:09 -0700

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# Objectives:
+# popular languages that Data Scientists use 
+# commonly used libraries used by Data Scientists
+# arithmetic expressions in Python
+
+# Some of the popular languages that Data Scientists use are:
+# 1 python
+# 2 R
+# 3 Java
+
+# Some of the commonly used libraries used by Data Scientists include:
+# panda,mathplotlib,TensorFlow and NumPy
+
+# Data Science Tools
+
+# Jupyter
+RStudio
+# tensorflow
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+(3*4)+5
+
+
+#  This will convert 200 minutes to hours by diving by 60
+
+# In[4]:
+
+
+200 / 60
+
+
+# ## Author 
+# Hector vejar
